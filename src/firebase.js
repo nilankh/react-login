ce9ce96fb8e8acacf65a667c8b309bcd6e6cc5e1
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-09-01 20:05:23 +0530

    done

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,5 @@
 import firebase from "firebase";
-const config = {
-  apiKey: "AIzaSyCCqx4r8Ab7gRVhbNoW9OOeLNwPik-CpwM",
-  authDomain: "fir-react-image-76851.firebaseapp.com",
-  databaseURL: "https://fir-react-image-76851.firebaseio.com",
-  projectId: "fir-react-image-76851",
-  storageBucket: "fir-react-image-76851.appspot.com",
-  messagingSenderId: "947341986898",
-  appId: "1:947341986898:web:6c2f6de92f919d3eaca38b",
-};
+const config = {};
 
 firebase.initializeApp(config);
 export default firebase;
