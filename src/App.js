ec5099a6d47d3d8a0d283b19f04f99a629418f83
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-09-01 19:32:52 +0530

    started wokring on uplading files

--- src/App.js
+++ src/App.js
@@ -1,68 +1,86 @@
-import React from "react";
+// import React from "react";
 
-import "./App.scss";
-import "./components/loginde/style.scss";
-import Login from "./components/loginde/Login";
-import Register from "./components/loginde/Register";
+// import "./App.scss";
+// import "./components/loginde/style.scss";
+// import Login from "./components/loginde/Login";
+// import Register from "./components/loginde/Register";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      isLogginActive: true,
-    };
-  }
+//     this.state = {
+//       isLogginActive: true,
+//     };
+//   }
 
-  changeState() {
-    const { isLogginActive } = this.state;
-    if (isLogginActive) {
-      this.rightSide.classList.remove("right");
-      this.rightSide.classList.add("left");
-    } else {
-      this.rightSide.classList.remove("left");
-      this.rightSide.classList.add("right");
-    }
-    this.setState((prevState) => ({
-      isLogginActive: !prevState.isLogginActive,
-    }));
-  }
+//   changeState() {
+//     const { isLogginActive } = this.state;
+//     if (isLogginActive) {
+//       this.rightSide.classList.remove("right");
+//       this.rightSide.classList.add("left");
+//     } else {
+//       this.rightSide.classList.remove("left");
+//       this.rightSide.classList.add("right");
+//     }
+//     this.setState((prevState) => ({
+//       isLogginActive: !prevState.isLogginActive,
+//     }));
+//   }
+//   render() {
+//     const { isLogginActive } = this.state;
+//     const current = isLogginActive ? "Register" : "Login";
+//     const currentActive = isLogginActive ? "login" : "register";
+//     return (
+//       <div className="App">
+//         <div className="login">
+//           <div className="container">
+//             {isLogginActive && (
+//               <Login containerRef={(ref) => (this.current = ref)} />
+//             )}
+//             {!isLogginActive && (
+//               <Register containerRef={(ref) => (this.current = ref)} />
+//             )}
+//           </div>
+//           <RightSide
+//             current={current}
+//             containerRef={(ref) => (this.rightSide = ref)}
+//             onClick={this.changeState.bind(this)}
+//           />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+// const RightSide = (props) => {
+//   return (
+//     <div
+//       className="right-side"
+//       ref={props.containerRef}
+//       onClick={props.onClick}
+//     >
+//       <div className="inner-container">
+//         <div className="text">{props.current}</div>
+//       </div>
+//     </div>
+//   );
+// };
+// export default App;
+
+
+import React, { Component } from 'react'
+import firebase from './firebase'
+
+export class App extends Component {
   render() {
-    const { isLogginActive } = this.state;
-    const current = isLogginActive ? "Register" : "Login";
-    const currentActive = isLogginActive ? "login" : "register";
     return (
-      <div className="App">
-        <div className="login">
-          <div className="container">
-            {isLogginActive && (
-              <Login containerRef={(ref) => (this.current = ref)} />
-            )}
-            {!isLogginActive && (
-              <Register containerRef={(ref) => (this.current = ref)} />
-            )}
-          </div>
-          <RightSide
-            current={current}
-            containerRef={(ref) => (this.rightSide = ref)}
-            onClick={this.changeState.bind(this)}
-          />
-        </div>
+      <div>
+        <input type="file" onChange={} />
+        <button onClick={}>Save</button>
+        <img id="new-img"
       </div>
-    );
+    )
   }
 }
-const RightSide = (props) => {
-  return (
-    <div
-      className="right-side"
-      ref={props.containerRef}
-      onClick={props.onClick}
-    >
-      <div className="inner-container">
-        <div className="text">{props.current}</div>
-      </div>
-    </div>
-  );
-};
-export default App;
+
+export default App
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+const config = {
+  apiKey: "AIzaSyCCqx4r8Ab7gRVhbNoW9OOeLNwPik-CpwM",
+  authDomain: "fir-react-image-76851.firebaseapp.com",
+  databaseURL: "https://fir-react-image-76851.firebaseio.com",
+  projectId: "fir-react-image-76851",
+  storageBucket: "fir-react-image-76851.appspot.com",
+  messagingSenderId: "947341986898",
+  appId: "1:947341986898:web:6c2f6de92f919d3eaca38b",
+};
+
+firebase.initializeApp(config)
+export default firebase
