2ba8b7f9080e89e4f05bb9641c6b53a6c2706cb0
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-09-01 15:01:35 +0530

    folder set up done and created login and register

---
+++ src/components/login/register.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import loginImg from "../../login.svg";
+
+class register extends Component {
+  constructor(props) {
+    super(props);
+
+    
+  }
+
+  render() {
+    return (
+      <div className="base-container">
+        <div className="header">Register</div>
+        <div className="content">
+          <div className="image">
+            <img src={loginImg} />
+          </div>
+
+          <div className="form">
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input type="text" name="username" placeholder="username" />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" placeholder="email" />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" placeholder="password" />
+            </div>
+
+          </div>
+        </div>
+        <div className="footer">
+          <button type="submit" className="btn">
+            Register
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default register;
